15ee278b18f0ddbb9bb3449430c3b151235cb0ee
Marshal Wu <marshal.wu@gmail.com>
2022-03-23 14:45:27 +0800

    real swagger /health

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # simple swagger
 
-简单的技术原型，koa 下，使用本地 swagger.json 文件（petstore），生成 swagger ui.
+简单的技术原型，koa 下，使用本地 YAML，为 koa 下的 restful api 提供 swagger ui 操作界面。
 
 ## 怎么使用
-
-
 
 ```
 npm i
 node app.js
 ```
 
-访问： http://localhost:3000/swagger
+访问： http://localhost:3000/api
+
+在 web 界面中对 /health 执行 restful 操作
 
 ## 历史
 
-- v0.1.0 使用本地文件
-
+- v0.2.0 使用本地 yaml 文件，对 koa 中的/health 提供 swagger 界面操作
+- v0.1.0 使用本地 json 文件(petstore)
--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 import Koa from "koa";
-import { koaSwagger } from "koa2-swagger-ui";
-import path from "path";
-import serve from "koa-static";
 import mount from "koa-mount";
+import swagger from "swagger2";
+import swaggerKoa from "swagger2-koa";
 
-import { fileURLToPath } from "url";
+const swaggerDocument = swagger.loadDocumentSync("api.yaml");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const health = new Koa();
 
-const api_doc = new Koa();
-api_doc.use(serve(path.join(__dirname, "/api")));
-
-const swagger = new Koa();
-swagger.use(
-  koaSwagger({
-    routePrefix: "/", // host at /swagger instead of default /docs
-    swaggerOptions: {
-      url: "http://localhost:3000/api-doc/swagger.json", // example path to json
-    },
-  })
-);
+health.use(async (ctx, next) => {
+  ctx.body = {
+    status: "UP",
+  };
+  await next();
+});
 
 const app = new Koa();
-app.use(mount("/api-doc", api_doc));
-app.use(mount("/swagger", swagger));
+app.use(mount("/health", health));
+app.use(swaggerKoa.ui(swaggerDocument, "/api"));
 
 app.listen(3000);
+console.log("API started");
